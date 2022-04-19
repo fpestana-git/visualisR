6b629b995ddffc15530c62c03c266987adcbbbd8
fpestana-git <72916953+fpestana-git@users.noreply.github.com>
2022-04-19 09:25:36 +0200

    update

--- R/drawDimPlotLiger.R
+++ R/drawDimPlotLiger.R
@@ -7,22 +7,22 @@ drawDimPlotLiger <- function(ligerObject){
   
   a <- umap_plots[[1]] + NoLegend()
   
-  ggsave(plot =a,filename = paste0("Results/DimPlot/", format(Sys.time(), "%Y%m%d_%H%M%S"),"_integrated.png"),         
+  ggsave(plot =a,filename = paste0("../Results/DimPlot/", format(Sys.time(), "%Y%m%d_%H%M%S"),"_integrated.png"),         
          width = 4,
          height = 4,
          units = "in",limitsize = FALSE)
-  ggsave(plot = a,filename = paste0("Results/DimPlot/", format(Sys.time(), "%Y%m%d_%H%M%S"),"_integrated.pdf"),         
+  ggsave(plot = a,filename = paste0("../Results/DimPlot/", format(Sys.time(), "%Y%m%d_%H%M%S"),"_integrated.pdf"),         
          width = 4,
          height = 4,
          units = "in",limitsize = FALSE)
   
   cluster <- plotByDatasetAndCluster(object = ligerObject,axis.labels = c("UMAP_1","UMAP_2")) +NoLegend()
   
-  ggsave(plot = cluster,filename = paste0( "Results/DimPlot/", format(Sys.time(), "%Y%m%d_%H%M%S"),"_integrated.png"),         
+  ggsave(plot = cluster,filename = paste0( "../Results/DimPlot/", format(Sys.time(), "%Y%m%d_%H%M%S"),"_integrated.png"),         
          width = 4,
          height = 4,
          units = "in",limitsize = FALSE)
-  ggsave(plot = cluster,filename = paste0( "Results/DimPlot/", format(Sys.time(), "%Y%m%d_%H%M%S"),"_integrated.pdf"),         
+  ggsave(plot = cluster,filename = paste0( "../Results/DimPlot/", format(Sys.time(), "%Y%m%d_%H%M%S"),"_integrated.pdf"),         
          width = 4,
          height = 4,
          units = "in",limitsize = FALSE)
