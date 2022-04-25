3639c8c1593d0cf815443fafa14e6457b598963c
fpestana-git <72916953+fpestana-git@users.noreply.github.com>
2022-04-25 10:59:53 +0200

    Update spatial feature plot

--- R/drawSpatialFeaturePlot.R
+++ R/drawSpatialFeaturePlot.R
@@ -1,40 +1,55 @@
 #' Draw Spatial Feature Plot for given genes
 #' 
-#' Function that generates spatial feature plots for a given gene and sample
+#' Function that generates spatial feature plots for a given gene. Can generate for single samples or recursively to all samples
 #' 
-#' @param dataset TEST
+#' @param spatialObject spatial Seurat object to use
+#' @param featureValue name of the feature to plot
+#' @param recursiveSection if plots should be made for all sections (default FALSE)
+#' @param sectionID if recursiveSection is FALSE, then a specific section ID should be defined (default NULL)
 #' 
 #' @export drawSpatialFeaturePlot
 
-drawSpatialFeaturePlot <- function(features,dataset,metadata, subsample = FALSE){
-  if (subsample != F) {
-    metadata <- metadata[which(metadata$orig.ident %in% subsample),]
-    dataset <- dataset[,metadata$Name]
-  }
-  # Create spatial Seurat
-  spatialSeurat <- createSpatialSeurat(sampleCounts = dataset,sampleCoordinates = metadata)
-  spatialSeurat@assays[["Spatial"]]@scale.data <- spatialClustering_norm@assays[["RNA"]]@scale.data
-  
-  # 
-  spatialSeurat <- AddMetaData(object = spatialSeurat, metadata = metadata)
-  plot <- SpatialFeaturePlot(spatialSeurat,features = features,stroke = 0,pt.size.factor = 2,slot = "scale.data")+ ggplot2::scale_fill_continuous(low = "gray95", high = "Black") 
+drawSpatialFeaturePlot <- function(spatialObject, featureValue, recursiveSection = FALSE,sectionID = NULL){
+  # Setup Ident as section ID 
+  Idents(object = spatialObject) <- spatialObject@meta.data[["sectionID"]]
   
   # Create folder
   dir.create(file.path("../Results/SpatialFeaturePlot/"),recursive = T,showWarnings = F)
   
-  
-  ggsave(plot = plot,
-         filename = paste0("../Results/SpatialFeaturePlot/",format(Sys.time(), "%Y%m%d_%H%M%S"),"_",features,".png"),
-         width = 5,
-         height = 7.5,
-         units = "in",limitsize = FALSE)
-  ggsave(plot = plot,
-         filename = paste0("../Results/SpatialFeaturePlot/",format(Sys.time(), "%Y%m%d_%H%M%S"),"_",features,".pdf"),
-         width = 5,
-         height = 7.5,
-         units = "in",limitsize = FALSE)
-  print(plot)
-  
-  
-  
+  # Make spatial dimplot either for all sections or for a specific section
+  if (recursiveSection) {
+    for (i in levels(spatialObject)) {
+      # Subset section
+      individualSection <- subset(x  = spatialObject,idents = i)
+      # Make plot
+      individualSection <- SpatialFeaturePlot(object = individualSection,
+                                              features = featureValue,
+                                              stroke = 0, 
+                                              pt.size.factor = 2) + ggplot2::scale_fill_continuous(low = "gray95", high = "Black") + NoLegend()
+      # Save plot
+      savePlots(plotObject = individualSection,
+                plotTypeValue = "SpatialFeaturePlot",
+                widthValue = 5,
+                heightValue = 7.5,
+                extraInfoValue = paste0("_",featureValue,"_",i))
+      
+    }
+    
+  }else {
+    # Subset section
+    individualSection <- subset(x  = spatialObject,idents = sectionID)
+    # Make plot
+    individualSection <- SpatialFeaturePlot(object = individualSection,
+                                            features = featureValue,
+                                            stroke = 0, 
+                                            pt.size.factor = 2) + ggplot2::scale_fill_continuous(low = "gray95", high = "Black") + NoLegend()
+    # Save plot
+    savePlots(plotObject = individualSection,
+              plotTypeValue = "SpatialFeaturePlot",
+              widthValue = 5,
+              heightValue = 7.5,
+              extraInfoValue = paste0("_",featureValue,"_",sectionID))
+  }
+  # Return plot
+  print(individualSection)
 }
